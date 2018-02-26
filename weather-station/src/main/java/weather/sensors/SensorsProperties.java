653c2696233a775db40f7ba23ca972e9387a8f8c
Michal Samujlo <michal.samujlo@gmail.com>
2018-02-26 23:09:15 +0100

    Add missing getters and setters

--- weather-station/src/main/java/weather/sensors/SensorsProperties.java
+++ weather-station/src/main/java/weather/sensors/SensorsProperties.java
@@ -4,6 +4,24 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("sensors")
 public class SensorsProperties {
+
     private double minTemperature;
     private double maxTemperature;
+
+    public double getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(double minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public double getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(double maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
 }
